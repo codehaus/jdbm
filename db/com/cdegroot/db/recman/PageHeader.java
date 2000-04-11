459b6ae9571ad4edcf64741edb7b8ba31330e3d6
boisvert <boisvert@4aaa5f03-64ec-0310-a9ff-a46452e5749d>
2000-04-11 06:05:46 +0000

    Constructor and SIZE now protected

--- db/com/cdegroot/db/recman/PageHeader.java
+++ db/com/cdegroot/db/recman/PageHeader.java
@@ -1,5 +1,5 @@
 /*
- *  $Id: PageHeader.java,v 1.1 2000/04/03 12:13:48 cdegroot Exp $
+ *  $Id: PageHeader.java,v 1.2 2000/04/11 06:05:46 boisvert Exp $
  *
  *  PageHeader.java
  *
@@ -7,8 +7,8 @@
  *  Copyright (C) 1999, 2000 Cees de Groot <cg@cdegroot.com>
  *
  *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Library General Public License 
- *  as published by the Free Software Foundation; either version 2 
+ *  modify it under the terms of the GNU Library General Public License
+ *  as published by the Free Software Foundation; either version 2
  *  of the License, or (at your option) any later version.
  *
  *  This library is distributed in the hope that it will be useful,
@@ -16,8 +16,8 @@
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Library General Public License for more details.
  *
- *  You should have received a copy of the GNU Library General Public License 
- *  along with this library; if not, write to the Free Software Foundation, 
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; if not, write to the Free Software Foundation,
  *  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  */
 package com.cdegroot.db.recman;
@@ -28,16 +28,16 @@ import java.io.*;
  *  This class represents a page header. It is the common superclass for
  *  all different page views.
  */
-class PageHeader implements BlockView {
+public class PageHeader implements BlockView {
     // offsets
     private static final short O_MAGIC = 0; // short magic
     private static final short O_NEXT = Magic.SZ_SHORT;  // long next
     private static final short O_PREV = O_NEXT + Magic.SZ_LONG; // long prev
-    static final int SIZE = O_PREV + Magic.SZ_LONG;
-    
+    protected static final short SIZE = O_PREV + Magic.SZ_LONG;
+
     // my block
-    BlockIo block;
-    
+    protected BlockIo block;
+
     /**
      *  Constructs a PageHeader object from a block
      *
@@ -45,14 +45,14 @@ class PageHeader implements BlockView {
      *  @throws IOException if the block is too short to keep the file
      *          header.
      */
-    PageHeader(BlockIo block) {
+    protected PageHeader(BlockIo block) {
 	initialize(block);
-	if (!magicOk()) 
-	    throw new Error("CRITICAL: page header magic for block " 
-			    + block.getBlockId() + " not OK " 
+	if (!magicOk())
+	    throw new Error("CRITICAL: page header magic for block "
+			    + block.getBlockId() + " not OK "
 			    + getMagic());
     }
-    
+
     /**
      *  Constructs a new PageHeader of the indicated type. Used for newly
      *  created pages.
@@ -84,30 +84,30 @@ class PageHeader implements BlockView {
      */
     private boolean magicOk() {
 	int magic = getMagic();
-	return magic >= Magic.BLOCK 
+	return magic >= Magic.BLOCK
 	    && magic <= (Magic.BLOCK + Magic.FREEPHYSIDS_PAGE);
     }
 
     /**
-     *  For paranoia mode 
+     *  For paranoia mode
      */
     protected void paranoiaMagicOk() {
 	if (!magicOk())
-	    throw new Error("CRITICAL: page header magic not OK " 
+	    throw new Error("CRITICAL: page header magic not OK "
 			    + getMagic());
     }
-    
+
     /** Returns the magic code */
     short getMagic() {
 	return block.readShort(O_MAGIC);
     }
-    
+
     /** Returns the next block. */
     long getNext() {
 	paranoiaMagicOk();
 	return block.readLong(O_NEXT);
     }
-    
+
     /** Sets the next block. */
     void setNext(long next) {
 	paranoiaMagicOk();
@@ -119,7 +119,7 @@ class PageHeader implements BlockView {
 	paranoiaMagicOk();
 	return block.readLong(O_PREV);
     }
-    
+
     /** Sets the previous block. */
     void setPrev(long prev) {
 	paranoiaMagicOk();
