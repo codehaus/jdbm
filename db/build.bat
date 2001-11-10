1676db6fc5164898d91b45f6b068f9a8fb131553
nobody <nobody@4aaa5f03-64ec-0310-a9ff-a46452e5749d>
2001-11-10 19:59:41 +0000

    This commit was manufactured by cvs2svn to create tag 'V0_13'.

--- db/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo BUILDING JDBM
-REM $Id: build.bat,v 1.1 2000/04/12 14:17:07 boisvert Exp $
-set JAVA=%JAVA_HOME%\bin\java
-set cp=%CLASSPATH%
-for %%i in (lib\*.jar) do call cp.bat %%i
-set CP=%JAVA_HOME%\lib\tools.jar;%CP%
-%JAVA% -classpath %CP% -Dant.home=lib org.apache.tools.ant.Main %1 %2 %3 %4 %5 %6 -buildfile build.xml
-
