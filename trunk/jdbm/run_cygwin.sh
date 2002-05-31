b13bf659106cde6fe0ff34730eadf146e5034496
boisvert <boisvert@4aaa5f03-64ec-0310-a9ff-a46452e5749d>
2002-05-31 06:35:58 +0000

    Added shell scripts for Cygwin

---
+++ trunk/jdbm/run_cygwin.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# $Id: run_cygwin.sh,v 1.1 2002/05/31 06:35:58 boisvert Exp $
+
+if [ -z "$JAVA_HOME" ] ; then
+  JAVA=`which java`
+  if [ -z "$JAVA" ] ; then
+    echo "Cannot find JAVA. Please set your PATH."
+    exit 1
+  fi
+  JAVA_BIN=`dirname $JAVA`
+  JAVA_HOME=$JAVA_BIN/..
+fi
+
+JAVA=$JAVA_HOME/bin/java
+
+CLASSPATH=`echo lib/*.jar | tr ' ' ';'`";$CLASSPATH"
+CLASSPATH="$JAVA_HOME/lib/tools.jar;$CLASSPATH"
+CLASSPATH="$CLASSPATH;./build/classes"
+CLASSPATH="$CLASSPATH;./build/tests"
+
+$JAVA -classpath $CLASSPATH "$@"
